3f0ef9109b085c029d75aa23f4fca8414ad5783a
김수민 <melody000000@naver.com>
2026-08-04 08:46:18 +0900

    [프로그래머스] L0 - 두 수의 합 구하기 (#8)

---
+++ programmers/L0_기초입문/입문/008_두_수의_합_구하기.py
@@ -0,0 +1,11 @@
+# 두 수의 합 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
+# 알고리즘: 기초
+# 작성자: 김수민
+# 작성일: 2026. 08. 04. 08:46:17
+
+def solution(num1, num2):
+    answer = -1
+    answer = num1 + num2
+    return answer
