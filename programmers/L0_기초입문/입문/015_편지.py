90cf888b69c3fa2e99e569c4c4a0b8ec32b753a9
김수민 <melody000000@naver.com>
2026-08-05 08:46:48 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,15 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 김수민
+# 작성일: 2026. 08. 05. 08:46:47
+
+def solution(message):
+    answer = 0
+    list = []
+    for i in message:
+        # print(i)
+        list.append(i)
+        answer = len(list) * 2
+    return answer
